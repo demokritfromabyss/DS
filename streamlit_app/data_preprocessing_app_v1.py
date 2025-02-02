45cc8f25756cb4504505b2e6512304e0b3d45fec
Alexander Popov <141047515+demokritfromabyss@users.noreply.github.com>
2025-02-03 00:42:43 +0200

    Update data_preprocessing_app_v1.py

--- streamlit_app/data_preprocessing_app_v1.py
+++ streamlit_app/data_preprocessing_app_v1.py
@@ -59,7 +59,10 @@ def main():
     data = st.session_state.processed_data.copy()
     if data is not None and not data.empty:
         max_rows = max(10, min(len(data), 10000))
+        if max_rows > 10:
         row_count = st.slider("Select number of rows to process", min_value=10, max_value=max_rows, value=min(10, max_rows))
+    else:
+        row_count = max_rows
         data = data.iloc[:row_count]
     else:
         st.warning("Dataset is empty or not loaded properly.")
